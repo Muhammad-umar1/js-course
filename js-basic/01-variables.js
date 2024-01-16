e326d615a262fcab2a32e2bb1c995cc4de6b48b7
Muhammad umar <79635447+Muhammad-umar1@users.noreply.github.com>
2024-01-16 19:22:06 +0000

    1st day javascript by umar

---
+++ js-basic/01-variables.js
@@ -0,0 +1,10 @@
+const umar="1029";
+let lhan="umar"
+let notDefined;
+var notDefined2=null;
+var idUser = "i1222"
+jee = "han"
+
+/*Not to use var keyword because of problem in block and function scope */
+
+console.table([umar, lhan, idUser, jee, notDefined, notDefined2])
---
+++ js-basic/02-datatypes.js
@@ -0,0 +1,22 @@
+"use stirct"; //treat all js code as newer version
+
+// alert(2+4); // can not use it because we are using Nodejs not browser compiler
+
+ //Data Types
+
+ //primitive data types::
+
+ //Number  => 2 to the power of 53
+ // bigint
+ // string => ""  
+ // boolean => true/false
+ //null => standalone value // representation of empty value its not like empty string we can assign it as null
+ // undefined =>   no value assign 
+ //symbols => used for uniqueness
+
+
+// non-primitive data types
+ //Objects
+
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); //object
