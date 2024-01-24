9d8f985f36e1eb7f785da95417d7187c71d77237
Muhammad umar <79635447+Muhammad-umar1@users.noreply.github.com>
2024-01-24 05:48:33 +0000

    Operaations in js

---
+++ js_basic/04_operations.js
@@ -0,0 +1,58 @@
+// ***********Operations*************
+
+//(1):******** OPERATION ON NUMBERS ***********
+
+// console.log(2 + 2); // 4
+// console.log(2 - 2); // 0
+// console.log(2 * 4); // 8
+// console.log(2 ** 4); // 16
+// console.log(2 / 2); // 1
+// console.log(3 % 2); // 1
+
+
+//(2):******** OPERATION ON STRING ***********
+
+// console.log("Muhammad" + " Umar"); // Muhammad Umar
+
+// let str1 = "3";
+// let str2 = "2";
+// let strSum = str1 + str2;
+// let strSub = str1 - str2;
+
+// console.log(strSum);    // 32
+// console.log(typeof strSum);   // string
+// console.log(strSub);  // 1
+// console.log(typeof(strSub));  //number
+
+// console.log("2" + 4); // 24 
+// console.log(typeof("2" + 4)); //string
+// console.log(2 + 2 + "2"); //  42 string
+// console.log(("2" + 2 + 2)); //  222 string
+// console.log(("2" + (2 + 2))); //  24 string
+// console.log(2 + "2" + 2); //  222 string
+
+// console.log(( 2 - "2")); // 0 number
+// console.log("2" - "2"); // 0 number
+// console.log("2" - 2); // 0 number
+// console.log("2" - 2 + 4); // 4 number
+// console.log(5 - 2 + "4"); // 34 number
+
+
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 3 + 4;
+
+
+// let num = 200;
+// let numPreInc = ++num;
+// let numPostInc = num++;
+
+// this two gives us the same value but there is difference check docum..
+
+// console.log(numPreInc);  // 201
+// console.log(numPostInc); //201 
