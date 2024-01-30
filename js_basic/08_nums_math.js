9bc1fb8b51f4a438b8ebd7e35a7df72c4961dfcc
Muhammad umar <79635447+Muhammad-umar1@users.noreply.github.com>
2024-01-30 06:42:51 +0000

    numer and Math

---
+++ js_basic/08_nums_math.js
@@ -0,0 +1,34 @@
+//+++++++++++=>  Numbers <=++++++++++++
+
+
+// const numb = new Number(100);
+
+// console.log(numb);
+// console.log(numb.toFixed(3)); // 100.000
+// console.log(numb.toString().includes("0")); // change to string and apply string methods
+
+// const num = 124.783
+// console.log(num.toPrecision(4)); // it gives us precise value 124.8
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString("en-In"));
+
+
+// +++++++++++=>  Maths <=++++++++++++
+
+// console.log(Math.abs(-333)); // Absolute
+// console.log(Math.min(-333, 0.00030)); // -333
+// console.log(Math.max(-333, 0.00030)); // 0.0003
+// console.log(Math.round(33.71)); // roundof
+// console.log(Math.ceil(33.71)); // roundof to top 34
+// console.log(Math.floor(33.71)); // roundof to bottom 33
+// console.log(Math.sqrt(3)); // square root of number
+// console.log(Math.pow(3,3)); // power of numb
+// console.log(Math.floor(Math.random()*10 + 1)); // it will give min value of 1 and the max will be 10
+
+// const min = 10
+// const max = 20
+// const random = Math.random();
+
+// let result1 = Math.floor(random * (max - min + 1)) + min  // it will give min value of 1 and the max will be 10
+// console.log("result1: "+ result1 );
